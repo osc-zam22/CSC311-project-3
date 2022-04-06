cec3ed5a766a1d4b3fcd256ad5352198b16e74c0
Oscar Zambrano <oscar@techexchange.in>
2022-04-05 21:24:23 -0700

    addded below 50k and half the between 50-80k

--- BST.java
+++ BST.java
@@ -223,14 +223,32 @@ class BST<Key extends Comparable<? super Key>, E extends Comparable<? extends E>
 
     public static void binarySearchBelowThreshold(BSTNode<Integer , String> root , int numBelow)
     {
-        if (root.key().compareTo(numBelow) > 0 )
+        if(root == null)
         {
+            return ;
+        }
+        if(root.key().compareTo(numBelow) <= 0)
+        {
+            System.out.print(root.toString() + "\n");
+        }
+        binarySearchBelowThreshold(root.left(), numBelow);
+        binarySearchBelowThreshold(root.right(), numBelow);
+    }
+
+    public static void betweenThreshold(BSTNode<Integer , String> root , int lowerBound, int upperBound )
+    {
+        {
+            if(root == null)
+            {
+                return ;
+            }
+            if(root.key() )
+            {
+                System.out.print(root.toString() + "\n");
+            }
             binarySearchBelowThreshold(root.left(), numBelow);
-        }
-        else if(root.key().compareTo(numBelow) <= 0 )
-        {
-            System.out.println();
-        }
+            binarySearchBelowThreshold(root.right(), numBelow);
+        } 
     }
 
 
